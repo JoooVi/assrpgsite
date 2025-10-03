1b066dabc378022875bb8b50ee93df8e38ad024c
João Vitor Grigorio Simoes <109753917+JoooVi@users.noreply.github.com>
2025-10-03 16:12:10 -0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://assrpgsite.railway.iassrpgsite-be-production.up.railway.app/api', // Substitua pela URL do seu backend no Railway
+  baseURL: 'https://assrpgsite-be-production.up.railway.app/api', // Substitua pela URL do seu backend no Railway
 });
 
 export default api;
